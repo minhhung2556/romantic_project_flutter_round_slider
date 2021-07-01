2c2b410dc5068161e294e9bf7ad754c45d36c8a3
Hung Luong Do Minh <minhhung2556@gmail.com>
2021-07-01 19:47:20 +0700

    add dart doc

--- lib/src/round_slider.dart
+++ lib/src/round_slider.dart
@@ -8,24 +8,54 @@ import 'math.dart';
 final _kAngleTween = Tween<double>(begin: 0, end: 360);
 final _kValueTween = Tween<double>(begin: 0, end: 1);
 
+/// Currently it supports only fixed [left] & [right]. Maybe change to [Alignment] in the future.
 enum RoundSliderAlignment { left, right }
 
+/// Style to draw the [RoundSlider]
 class RoundSliderStyle {
+  /// [stepLineCount] : count of lines that separate the circle.
   final int stepLineCount;
+
+  /// [borderColor] : color of the border circle.
   final Color borderColor;
+
+  /// [borderStroke] : stroke width of the border circle.
   final double borderStroke;
+
+  /// [glowDistance] : the distance that the glow gradient spreads.
   final double glowDistance;
+
+  /// [lineMargin] : margin from each line to the border circle.
   final double lineMargin;
+
+  /// [lineLengths] : describes 3 types of line length. The line at the index%10==0 uses lineLengths[2]. The line at the index%5==0 uses lineLengths[1], and other use lineLengths[0].
   final List<double> lineLengths;
+
+  /// [lineColor] : color of lines. Only use one color.
   final Color lineColor;
+
+  /// [lineStroke] : stroke width of lines.
   final double lineStroke;
+
+  /// [friction] : The friction of sliding. As big as hard.
   final double friction;
+
+  /// [radius] : radius of the circle.
   final double radius;
+
+  /// [visibleFactor] : [0...1], factor of the circle diameter that is visible.
   final double visibleFactor;
+
+  /// [alignment] : See also [RoundSliderAlignment].
   final RoundSliderAlignment alignment;
+
+  /// [glowColorStops] : colorStops to draw the glow gradient, see also [ui.Gradient].
   final List<Color> glowColorStops;
+
+  /// [glowStops] : stops to draw the glow gradient, see also [ui.Gradient].
   final List<double> glowStops;
 
+  /// Create a RoundSliderStyle, it has a default style already.
   const RoundSliderStyle({
     this.glowColorStops: const [
       Colors.transparent,
@@ -56,6 +86,7 @@ class RoundSliderStyle {
             visibleFactor <= 1 &&
             visibleFactor > 0);
 
+  /// it is used to compare between styles.
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
@@ -76,6 +107,7 @@ class RoundSliderStyle {
           glowColorStops == other.glowColorStops &&
           glowStops == other.glowStops;
 
+  /// it is used to compare between styles.
   @override
   int get hashCode =>
       stepLineCount.hashCode ^
@@ -94,12 +126,21 @@ class RoundSliderStyle {
       glowStops.hashCode;
 }
 
+/// A Round Slider that is drawn by [RoundSliderStyle].
 class RoundSlider extends StatefulWidget {
+  /// [value] : [0...1]
   final double value;
+
+  /// [onChanged] : callback whenever the [value] is changed.
   final Function(double value) onChanged;
+
+  /// [style] : defines how this RoundSlider shows.
   final RoundSliderStyle style;
+
+  /// [animationDuration] : the animation duration that runs to change from the current value to a new value.
   final Duration animationDuration;
 
+  /// Create a RoundSlider.
   const RoundSlider({
     Key? key,
     this.style: const RoundSliderStyle(),
@@ -116,8 +157,8 @@ class RoundSlider extends StatefulWidget {
 class _RoundSliderState extends State<RoundSlider>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  double valueT = 0;
-  late Tween<double> valueChangedTween;
+  double _valueT = 0;
+  late Tween<double> _valueChangedTween;
 
   @override
   void initState() {
@@ -135,11 +176,11 @@ class _RoundSliderState extends State<RoundSlider>
 
   void _updateFromWidget() {
     var t = widget.value;
-    if (t != valueT) {
-      valueChangedTween = Tween(begin: valueT, end: t);
+    if (t != _valueT) {
+      _valueChangedTween = Tween(begin: _valueT, end: t);
       _controller.forward(from: 0);
     }
-    valueT = t;
+    _valueT = t;
   }
 
   @override
@@ -161,10 +202,10 @@ class _RoundSliderState extends State<RoundSlider>
     width = widget.style.visibleFactor * height;
 
     var t = _controller.isAnimating
-        ? valueChangedTween.evaluate(
+        ? _valueChangedTween.evaluate(
             CurveTween(curve: Curves.fastLinearToSlowEaseIn)
                 .animate(_controller))
-        : valueT;
+        : _valueT;
     var angle = _kAngleTween.transform(t);
     return GestureDetector(
       onPanUpdate: (e) {
@@ -173,12 +214,12 @@ class _RoundSliderState extends State<RoundSlider>
           var factor =
               (widget.style.alignment == RoundSliderAlignment.right ? -1 : 1) *
                   (e.delta.dy / (height * widget.style.friction));
-          valueT = math.min(math.max(valueT + factor, 0), 1);
+          _valueT = math.min(math.max(_valueT + factor, 0), 1);
         });
       },
       onPanEnd: (e) {
         setState(() {
-          widget.onChanged(_kValueTween.transform(valueT));
+          widget.onChanged(_kValueTween.transform(_valueT));
         });
       },
       child: CustomPaint(
@@ -193,8 +234,10 @@ class _RoundSliderState extends State<RoundSlider>
 }
 
 class _RoundSliderPainter extends CustomPainter {
-  /// angle in degree
+  /// [angle] in degree
   final double angle;
+
+  /// [style] : See [RoundSliderStyle]
   RoundSliderStyle style;
 
   _RoundSliderPainter({
